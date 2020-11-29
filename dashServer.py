7970c957b66aefd816c2d04514e5593d4e1231b1
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-11-29 19:42:15 +0100

    [add] network training

--- dashServer.py
+++ dashServer.py
@@ -7,6 +7,8 @@ import dash_bootstrap_components as dbc
 import numpy as np
 import plotly.graph_objects as go
 import classifier
+import network
+import random
 
 
 class dashServer:
@@ -84,63 +86,16 @@ class dashServer:
         ])
 
     def __configCallbacks(self):
-        print('config callbacks')
-
-        # @self.app.callback(
-        #     [Output('contour-plot', 'figure'),
-        #      Output('btn-train-and-plot', 'disabled')],
-        #     [Input('btn-generate-classifiers', 'n_clicks')],
-        #     [State('input-modes-per-classifier', 'value'),
-        #      State('input-samples-per-mode', 'value')],
-        # )
-        # def gernerateClassifiers(btn_clicks, modesPerClassifier, samplesPerMode):
-        #     if (btn_clicks == 0 or modesPerClassifier in [None, 0] or samplesPerMode in [None, 0]):
-        #         return [{}, True]
-
-        #     c1 = classifier.Classifier(modesPerClassifier, samplesPerMode)
-        #     c2 = classifier.Classifier(modesPerClassifier, samplesPerMode)
-
-        #     self.classifier1 = c1
-        #     self.classifier2 = c2
-
-        #     scatter1 = go.Scatter(
-        #         x=c1.getAllSamples()[0],
-        #         y=c1.getAllSamples()[1],
-        #         name=0,
-        #         mode='markers'
-        #     )
-
-        #     scatter2 = go.Scatter(
-        #         x=c2.getAllSamples()[0],
-        #         y=c2.getAllSamples()[1],
-        #         name=1,
-        #         mode='markers'
-        #     )
-
-        #     fig = go.Figure(data=[scatter1, scatter2])
-        #     fig.update_xaxes(range=[-.1, 1.1])
-        #     fig.update_yaxes(range=[-.1, 1.1])
-
-        #     return [fig, False]
-
-        # @self.app.callback(
-        #     Output('contour-plot', 'figure'),
-        #     [Input('btn-train-and-plot', 'n_clicks')],
-        #     [State('input-network-model', 'value'),
-        #      State('input-epochs', 'value')],
-        # )
-        # def trainAndPlot(btn_clicks, networkModel, epochs):
-        #     print('btn-train-and-plot', btn_clicks, networkModel, epochs)
-        #     return {}
-
         @self.app.callback(
             Output('contour-plot', 'figure'),
             [Input('btn-generate-classifiers', 'n_clicks_timestamp'),
              Input('btn-train-and-plot', 'n_clicks_timestamp')],
             [State('input-modes-per-classifier', 'value'),
-             State('input-samples-per-mode', 'value')]
+             State('input-samples-per-mode', 'value'),
+             State('input-network-model', 'value'),
+             State('input-epochs', 'value')]
         )
-        def whichButtonWasClicked(btn_generate_timestamp, btn_training_timestamp, modesPerClassifier, samplesPerMode):
+        def whichButtonWasClicked(btn_generate_timestamp, btn_training_timestamp, modesPerClassifier, samplesPerMode, networkModel, epochs):
             operation = ''
 
             if (isinstance(btn_generate_timestamp, str) and isinstance(btn_training_timestamp, int)):
@@ -184,6 +139,41 @@ class dashServer:
 
                 return fig
 
+            elif (operation == 'training'):
+                print('training', networkModel, epochs)
+
+                if (self.classifier1 is None or self.classifier2 is None):
+                    print('classifiers are not defined')
+                    return {}
+
+                if (networkModel in [None, ''] or epochs is None or epochs <= 0):
+                    print('will not do the training')
+                    return {}
+
+                pointsFromC1 = self.classifier1.getAllPoints()
+                pointsFromC2 = self.classifier2.getAllPoints()
+
+                inputOutput1 = list(
+                    map(lambda point: [point, [0, 1]], pointsFromC1))
+                inputOutput2 = list(
+                    map(lambda point: [point, [1, 0]], pointsFromC2))
+
+                inputsOutputs = inputOutput1 + inputOutput2
+
+                n = network.Network([1, 3, 2])
+                print('b2', n)
+                for i in range(epochs):
+                    random.shuffle(inputsOutputs)
+                    for io in inputsOutputs:
+                        n.train(io[0], io[1], 2)
+
+                for i in range(len(inputsOutputs)):
+                    print('expected output:', inputsOutputs[i][1])
+                    print('output:         ', n.evaluate(inputsOutputs[i][0]))
+
+                return {}
+            else:
+                raise Exception('Operation not detected: ' + operation)
             print(operation)
             return {}
 
